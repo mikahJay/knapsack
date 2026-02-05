b25c9802b8dd3a8b3728846f49574fd18bf0b00b
mikahJay <mikah.trent@gmail.com>
2026-02-05 17:56:46 -0600

    working on env-config still

--- apps/web-app/src/index.jsx
+++ apps/web-app/src/index.jsx
@@ -4,8 +4,23 @@ import App from './App'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
 
-createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-)
+function ensureRuntimeEnv() {
+  if (typeof window !== 'undefined' && window.__ENV__) {
+    return Promise.resolve()
+  }
+  return new Promise(resolve => {
+    const script = document.createElement('script')
+    script.src = '/env-config.js'
+    script.onload = () => resolve()
+    script.onerror = () => resolve()
+    document.head.appendChild(script)
+  })
+}
+
+ensureRuntimeEnv().finally(() => {
+  createRoot(document.getElementById('root')).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  )
+})
