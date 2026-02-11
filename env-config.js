7c19c9124d1b8f579db42fb226047513e4bda97a
mikahJay <mikah.trent@gmail.com>
2026-02-10 19:44:28 -0600

    fix: use domain name instead of ALB DNS for API endpoints

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 ﻿window.__ENV__ = {
-  VITE_API_NEED: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/need",
-  VITE_API_RESOURCE: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/resources",
-  VITE_API_AUTH: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/auth",
+  VITE_API_NEED: "https://knap-sack.com/need",
+  VITE_API_RESOURCE: "https://knap-sack.com/resources",
+  VITE_API_AUTH: "https://knap-sack.com/auth",
   VITE_GOOGLE_CLIENT_ID: "385658087920-b9muielg4oe4s57sfq4s9dirlu155av8.apps.googleusercontent.com"
 };
 
