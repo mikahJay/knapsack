4e5144441cf6d222e2528e96c0e8f1090b63660b
mikahJay <mikah.trent@gmail.com>
2026-02-10 19:25:12 -0600

    fix: update ALB DNS name and Google client ID in runtime config

--- env-config.js
+++ env-config.js
@@ -1,7 +1,7 @@
 ﻿window.__ENV__ = {
-  VITE_API_NEED: "https://knapsack-alb-test-2101219521.us-east-2.elb.amazonaws.com/need",
-  VITE_API_RESOURCE: "https://knapsack-alb-test-2101219521.us-east-2.elb.amazonaws.com/resources",
-  VITE_API_AUTH: "https://knapsack-alb-test-2101219521.us-east-2.elb.amazonaws.com/auth",
-  VITE_GOOGLE_CLIENT_ID: ""
+  VITE_API_NEED: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/need",
+  VITE_API_RESOURCE: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/resources",
+  VITE_API_AUTH: "https://knapsack-alb-test-956249715.us-east-2.elb.amazonaws.com/auth",
+  VITE_GOOGLE_CLIENT_ID: "385658087920-b9muielg4oe4s57sfq4s9dirlu155av8.apps.googleusercontent.com"
 };
 
